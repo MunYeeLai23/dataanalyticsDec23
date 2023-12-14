0ca5cbe6216cfa6e02d5b98e79fd884b02445cad
MunYeeLai23 <153788566+MunYeeLai23@users.noreply.github.com>
2023-12-14 14:20:03 +0800

    Add files via upload

--- AdvertisingSVM.py
+++ AdvertisingSVM.py
@@ -11,9 +11,9 @@ This app predicts the **Advertising Sales**
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV = st.sidebar.slider('TV', 4.3, 7.9, 5.4)
-    Radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
-    Newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
+    TV = st.sidebar.slider('TV', 0.0, 296.0, 50.)
+    Radio = st.sidebar.slider('Radio', 0.0, 50.0, 25.0)
+    Newspaper = st.sidebar.slider('Newspaper', 0.0, 114.0, 1.3)
     data = {'TV': TV,
             'Radio': Radio,
             'Newspaper': Newspaper}
